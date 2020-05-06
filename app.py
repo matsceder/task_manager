6b5f270edaeab749df4ff756afcb3e34862f3955
matsceder <mats_ceder@hotmail.com>
2020-05-06 22:27:50 +0000

    Done with Putting all basic in place lesson

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_mananger'
+app.config["MONGO_URI"] = 'mongodb+srv://mec_88_ci_19:d24BCuRG7tgAD@myfirstcluster-htagx.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World... again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
