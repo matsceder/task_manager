a949187eea684cecbd37a5287caae22e196a3bce
matsceder <mats_ceder@hotmail.com>
2020-05-07 15:24:24 +0000

    Almost done with editing a task. save for day break in study

--- app.py
+++ app.py
@@ -9,13 +9,34 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_mananger'
 app.config["MONGO_URI"] = 'mongodb+srv://mec_88_ci_19:d24BCuRG7tgAD@myfirstcluster-htagx.mongodb.net/task_manager?retryWrites=true&w=majority'
 
+
 mongo = PyMongo(app)
 
 
 @app.route('/')
+@app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task, categories=all_categories)
 
 
 if __name__ == '__main__':
